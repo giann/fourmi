8fcb663eb67d295af0653bd7629bd0b7fd8e4164
Benoit Giannangeli <giann008@gmail.com>
2019-03-09 18:42:16 +0100

    .fourmi/tmp

--- example-fourmi.plan.lua
+++ example-fourmi.plan.lua
@@ -4,16 +4,21 @@ local fourmi   = require "fourmi"
 local builtins = require "fourmi.builtins"
 local plan     = fourmi.plan
 local task     = fourmi.task
+local __       = builtins.__
 local sh       = builtins.sh
 local ls       = builtins.task.ls
 local mv       = builtins.task.mv
+local shtask   = builtins.task.sh
 
 -- Define tasks
 
 local minify = task "minify"
     :description "Minify lua files with luamin"
     :perform(function(self, file)
-        local minifiedFile = os.tmpname() .. "_" .. file:match "([^/]*)$"
+        local minifiedFile = __"${HOME}/.fourmi/tmp/"
+            .. file
+                :match "([^/]*)$"
+                :gsub("%.lua$", ".min.lua")
 
         if not sh("luamin", "-f", file, ">", minifiedFile) then
             error("Could not create minified file at: " .. minifiedFile)
@@ -40,5 +45,11 @@ return {
         :task(
             ls("./fourmi", "%.lua$")
                 * (minify >> gzip >> mv "${HOME}/tmp-code")
+        ),
+
+    plan "clean"
+        :description "Cleanup"
+        :task(
+            shtask("rm", __"${HOME}/tmp-code/*.gz")
         )
 }
---
+++ init.lua
@@ -0,0 +1,1 @@
+local a={task=require"fourmi.task",plan=require"fourmi.plan",builtins=require"fourmi.builtins"}return a
